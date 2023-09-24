56d8c523275b37ffcfea8c76797a5863756190c6
MuslimaHabibullayevna <93874705+MuslimaHabibullayevna@users.noreply.github.com>
2023-09-24 17:30:43 +0300

    kod qoshildi
    python

---
+++ rep.py
@@ -0,0 +1,11 @@
+a = int(input("Birinchi raqamni kiriting: "))
+
+b = int(input("Ikkinchi raqamni kiriting: "))
+
+c = int(input("Uchinchi raqaqamni kiriting: "))
+
+d = int(input("To'rtinchi raqamni kiriting: "))
+
+e = (a + b) / (c + d)
+
+print("Javob:", e)
